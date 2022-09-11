fc3a00b7c0eb6e917c7ce8be23397ef9b715b508
Shan <zwh.20@qq.com>
2022-09-11 14:56:10 +0800

    包扫描

--- src/main/java/com/neusoft/elmboot/ElmBootApplication.java
+++ src/main/java/com/neusoft/elmboot/ElmBootApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableAutoConfiguration//开启自动化配置 由starter提供服务
-@SpringBootApplication
+
+@SpringBootApplication//开启自动化配置 由starter提供服务; 包扫描
 public class ElmBootApplication {
 public static void main(String[] args) {
 	SpringApplication.run(ElmBootApplication.class, args);
