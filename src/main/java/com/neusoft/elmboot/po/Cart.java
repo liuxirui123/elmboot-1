ef396226f667b0c3c9d61963a5676b9dc30e6e29
Shan <zwh.20@qq.com>
2022-09-11 14:51:06 +0800

    创建 po

---
+++ src/main/java/com/neusoft/elmboot/po/Cart.java
@@ -0,0 +1,75 @@
+package com.neusoft.elmboot.po;
+
+
+public class Cart {
+
+private Integer cartId;
+private Integer foodId;
+private Integer businessId;
+private String userId;
+private Integer quantity;
+
+private Food food;//多对一：所属食品
+
+private Business business;//多对一：所属商家
+
+public Cart() {
+
+}
+
+public Cart(String userId, Integer businessId) {
+	this.userId = userId;
+	this.businessId = businessId;
+}
+
+public Cart(String userId, Integer businessId, Integer foodId) {
+	this(userId, businessId);
+	this.foodId = foodId;
+}
+
+
+public void setCartId(Integer cartId) {
+	this.cartId = cartId;
+}
+
+public Integer getFoodId() {
+	return foodId;
+}
+
+
+public Integer getBusinessId() {
+	return businessId;
+}
+
+
+public String getUserId() {
+	return userId;
+}
+
+
+public Integer getQuantity() {
+	return quantity;
+}
+
+public void setQuantity(Integer quantity) {
+	this.quantity = quantity;
+}
+
+
+public void setFood(Food food) {
+	this.food = food;
+}
+
+
+public void setBusiness(Business business) {
+	this.business = business;
+}
+
+public void setUserId(String userId) {
+	this.userId = userId;
+}
+
+public void setBusinessId(Integer businessId) {
+	this.businessId = businessId;
+}
+}
