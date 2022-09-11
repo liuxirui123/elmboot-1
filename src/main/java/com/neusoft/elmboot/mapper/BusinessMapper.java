74c7d14c01b4f4111c807ce5b913c68f111f51e0
Shan <zwh.20@qq.com>
2022-09-11 15:07:01 +0800

    添加business

---
+++ src/main/java/com/neusoft/elmboot/mapper/BusinessMapper.java
@@ -0,0 +1,17 @@
+package com.neusoft.elmboot.mapper;
+
+import com.neusoft.elmboot.po.Business;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface BusinessMapper {
+
+@Select("select * from business where orderTypeId=#{orderTypeId} order by businessId")
+List<Business> listBusinessByOrderTypeId(Integer orderTypeId);
+
+@Select("select * from business where businessId=#{businessId}")
+Business getBusinessById(Integer businessId);
+}
