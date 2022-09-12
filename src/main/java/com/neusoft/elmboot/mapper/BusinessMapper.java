cdb3fb6aafb13cec5f6086ad435cbaf1df942b20
Shan <zwh.20@qq.com>
2022-09-12 10:31:29 +0800

    格式化

--- src/main/java/com/neusoft/elmboot/mapper/BusinessMapper.java
+++ src/main/java/com/neusoft/elmboot/mapper/BusinessMapper.java
@@ -8,10 +8,10 @@ import java.util.List;
 
 @Mapper
 public interface BusinessMapper {
-
-@Select("select * from business where orderTypeId=#{orderTypeId} order by businessId")
-List<Business> listBusinessByOrderTypeId(Integer orderTypeId);
-
-@Select("select * from business where businessId=#{businessId}")
-Business getBusinessById(Integer businessId);
+	
+	@Select("select * from business where orderTypeId=#{orderTypeId} order by businessId")
+	List<Business> listBusinessByOrderTypeId(Integer orderTypeId);
+	
+	@Select("select * from business where businessId=#{businessId}")
+	Business getBusinessById(Integer businessId);
 }
