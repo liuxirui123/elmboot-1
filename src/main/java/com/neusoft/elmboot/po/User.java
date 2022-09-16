9ebc794a92516f3245bd7a00a7aaeeaab3619992
Shan <zwh.20@qq.com>
2022-09-16 21:08:01 +0800

    UserMapper success

--- src/main/java/com/neusoft/elmboot/po/User.java
+++ src/main/java/com/neusoft/elmboot/po/User.java
@@ -1,59 +1,82 @@
 package com.neusoft.elmboot.po;
 
 public class User {
-	
-	private String userId;
-	private String password;
-	private String userName;
-	private Integer userSex;
-	private String userImg;
-	private Integer delTag;
-	
-	public String getUserId() {
-		return userId;
-	}
-	
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-	
-	public String getPassword() {
-		return password;
-	}
-	
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	public String getUserName() {
-		return userName;
-	}
-	
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	
-	public Integer getUserSex() {
-		return userSex;
-	}
-	
-	public void setUserSex(Integer userSex) {
-		this.userSex = userSex;
-	}
-	
-	public String getUserImg() {
-		return userImg;
-	}
-	
-	public void setUserImg(String userImg) {
-		this.userImg = userImg;
-	}
-	
-	public Integer getDelTag() {
-		return delTag;
-	}
-	
-	public void setDelTag(Integer delTag) {
-		this.delTag = delTag;
-	}
+
+    private String userId;
+    private String password;
+    private String userName;
+    private Integer userSex;
+    private String userImg;
+    private Integer delTag;
+
+    public User(String userId, String password) {
+        this.userId = userId;
+        this.password = password;
+    }
+
+    public User(String userId, String password, String userName, Integer userSex) {
+        this(userId, password);
+        this.userName = userName;
+        this.userSex = userSex;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId='" + userId + '\'' +
+                ", password='" + password + '\'' +
+                ", userName='" + userName + '\'' +
+                ", userSex=" + userSex +
+                ", userImg: 略" + '\'' +
+                ", delTag=" + delTag +
+                '}';
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Integer getUserSex() {
+        return userSex;
+    }
+
+    public void setUserSex(Integer userSex) {
+        this.userSex = userSex;
+    }
+
+    public String getUserImg() {
+        return userImg;
+    }
+
+    public void setUserImg(String userImg) {
+        this.userImg = userImg;
+    }
+
+    public Integer getDelTag() {
+        return delTag;
+    }
+
+    public void setDelTag(Integer delTag) {
+        this.delTag = delTag;
+    }
 }
