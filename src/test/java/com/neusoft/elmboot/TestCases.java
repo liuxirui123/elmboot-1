31a45742523ff0f062b956293b499dcf89bb5f75
Shan <zwh.20@qq.com>
2022-10-07 20:50:02 +0800

    add TestCases

---
+++ src/test/java/com/neusoft/elmboot/TestCases.java
@@ -0,0 +1,8 @@
+package com.neusoft.elmboot;
+
+public class TestCases {
+    public static int[] businessIds = {10001, 10002, 10003};
+    public static int[] businessIdsNOt = {0, -2, 90};
+    public static int[] businessOrderTypeIds = {1, 4, 5};
+    public static int[] businessOrderTypeIdsNot = {-1, -2, 10123};
+}
