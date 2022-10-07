33e571874632136203215dfc5d6702ef19df1f4f
Shan <zwh.20@qq.com>
2022-10-07 20:46:22 +0800

    add constructor

--- src/main/java/com/neusoft/elmboot/entity/BusinessEntity.java
+++ src/main/java/com/neusoft/elmboot/entity/BusinessEntity.java
@@ -2,10 +2,18 @@ package com.neusoft.elmboot.entity;
 
 
 import com.neusoft.elmboot.po.Business;
+import lombok.NoArgsConstructor;
 
+
+@NoArgsConstructor
 public class BusinessEntity extends Business {
 
     private Integer businessId;
+
+    public BusinessEntity(Integer businessId, String businessName, String businessAddress, String businessExplain, String businessImg, Integer orderTypeId, double starPrice, double deliveryPrice, String remarks) {
+        super(businessName, businessAddress, businessExplain, businessImg, orderTypeId, starPrice, deliveryPrice, remarks);
+        this.businessId = businessId;
+    }
 
     @Override
     public String toString() {
@@ -14,4 +22,5 @@ public class BusinessEntity extends Business {
                 super.toString() +
                 '}';
     }
+
 }
