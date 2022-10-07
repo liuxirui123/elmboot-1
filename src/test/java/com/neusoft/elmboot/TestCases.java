78415ef62c1c1e5905a4b80453dc9392f628db5c
Shan <zwh.20@qq.com>
2022-10-07 21:14:54 +0800

    add some testCases

--- src/test/java/com/neusoft/elmboot/TestCases.java
+++ src/test/java/com/neusoft/elmboot/TestCases.java
@@ -1,8 +1,11 @@
 package com.neusoft.elmboot;
 
 public class TestCases {
-    public static int[] businessIds = {10001, 10002, 10003};
-    public static int[] businessIdsNOt = {0, -2, 90};
+    public static int[] businessIds = {10001, 10002};
+    public static int[] businessIdsNOt = {0, -2, 90, 1232341235};
     public static int[] businessOrderTypeIds = {1, 4, 5};
-    public static int[] businessOrderTypeIdsNot = {-1, -2, 10123};
+    public static int[] businessOrderTypeIdsNot = {-1, -2, 10123, 0};
+
+    public static int[] foodIds = {1, 2, 3, 4, 5};
+    public static int[] foodIdsNot = {-1, -123, 12353152, 0};
 }
