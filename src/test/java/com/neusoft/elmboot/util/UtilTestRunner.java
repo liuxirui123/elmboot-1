60623a2897feacee7efa71684842dc5db4399db0
Shan <zwh.20@qq.com>
2022-10-07 20:47:10 +0800

    add UtilTestRunner

---
+++ src/test/java/com/neusoft/elmboot/util/UtilTestRunner.java
@@ -0,0 +1,9 @@
+package com.neusoft.elmboot.util;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({CommonUtilTest.class, ModelConverterUtilsTest.class})
+public class UtilTestRunner {
+}
