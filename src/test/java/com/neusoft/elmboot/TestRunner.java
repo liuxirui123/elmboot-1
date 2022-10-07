ccdd92f3c675bd6ce502a9ce345dae99bd8b11ef
Shan <zwh.20@qq.com>
2022-10-07 21:05:31 +0800

    improve TestRunner

--- src/test/java/com/neusoft/elmboot/TestRunner.java
+++ src/test/java/com/neusoft/elmboot/TestRunner.java
@@ -1,23 +1,12 @@
 package com.neusoft.elmboot;
 
-import org.apache.ibatis.session.SqlSession;
-import org.junit.Assert;
-import org.junit.Test;
+import com.neusoft.elmboot.util.UtilTestRunner;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 
+@RunWith(Suite.class)
+@Suite.SuiteClasses({UtilTestRunner.class})
 public class TestRunner {
 
-    @Test
-    public void testGetSqlSession() {
-        SqlSession sqlSession;
-        sqlSession = MyBatisUtil.getSqlSession();
-        Assert.assertNotNull(sqlSession);
-    }
-
-    @Test
-    public void TestSelect() {
-        SqlSession sqlSession = MyBatisUtil.getSqlSession();//sqlSession.selectOne最终结果与映射文件中所匹配的resultType类型
-
-        sqlSession.close();
-    }
 
 }
