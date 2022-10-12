0d67477df1166bea3a9c0de5c31a97e2b8858678
Shan <zwh.20@qq.com>
2022-10-12 13:42:53 +0800

    add tests

---
+++ src/test/java/com/neusoft/elmboot/request/ControllerTestRunner.java
@@ -0,0 +1,9 @@
+package com.neusoft.elmboot.request;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({BusinessControllerTest.class})
+public class ControllerTestRunner {
+}
